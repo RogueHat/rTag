a5a4159e020067f018d9b486265cb27f68f7f96a
CarloSchool <carlomiguelh@yahoo.com>
2013-02-07 13:27:40 -0600

    try

--- tagGame/src/network/Host.java
+++ tagGame/src/network/Host.java
@@ -10,31 +10,33 @@ public class Host {
     public static void main(String[] args) throws IOException {
 
 
-        ServerSocket servsock = new ServerSocket(15064);
+        ServerSocket servsock = new ServerSocket(12345);
         Socket sock = servsock.accept();
             long time = System.currentTimeMillis();
 
         OutputStream out = sock.getOutputStream();
-        FileInputStream fileInputStream = new FileInputStream("C:\\complete.rar");
+        //FileInputStream fileInputStream = new FileInputStream("C:\\complete.rar");
 
-        byte [] buffer = new byte[64*1024]; 
-        int bytesRead = 0;
-        long totalSent = 0;
-
-        while ( (bytesRead = fileInputStream.read(buffer)) != -1)
-        {
-            if (bytesRead > 0)
-            {   
-                out.write(buffer, 0, bytesRead);
-                totalSent += bytesRead;
-                System.out.println("sent " + totalSent);
-            }   
-        }
+        byte [] buffer = "0123456789".getBytes(); 
+    	out.write(buffer);
+        
+//        while ( (bytesRead = fileInputStream.read(buffer)) != -1)
+//        {
+//            if (bytesRead > 0)
+//            {   
+//                out.write(buffer, 0, bytesRead);
+//                totalSent += bytesRead;
+//                System.out.println("sent " + totalSent);
+//            }   
+//        }
 
         sock.close();
 
-        System.out.println("Sent " + totalSent + " bytes in "
-                + (System.currentTimeMillis() - time) + "ms.");
+        //System.out.println("Sent " + totalSent + " bytes in "
+        //        + (System.currentTimeMillis() - time) + "ms.");
+        
+        
+        
 
     }
 }
