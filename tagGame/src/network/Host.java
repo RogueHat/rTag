adeb938d9bc407c75a7074a8cb31e309b1a40f5d
CarloSchool <carlomiguelh@yahoo.com>
2013-02-13 13:05:31 -0600

    test 2

--- tagGame/src/network/Host.java
+++ tagGame/src/network/Host.java
@@ -8,35 +8,18 @@ public class Host {
 
 
     public static void main(String[] args) throws IOException {
+    	PrintWriter out = null;
+    	BufferedReader in = null;
+    	BufferedReader stdIn = new BufferedReader(new InputStreamReader(
+				System.in));
+		String userInput;
 
-
-        ServerSocket servsock = new ServerSocket(12345);
-        Socket sock = servsock.accept();
-            long time = System.currentTimeMillis();
-
-        OutputStream out = sock.getOutputStream();
-        //FileInputStream fileInputStream = new FileInputStream("C:\\complete.rar");
-
-        byte [] buffer = "0123456789".getBytes(); 
-    	out.write(buffer);
-        
-//        while ( (bytesRead = fileInputStream.read(buffer)) != -1)
-//        {
-//            if (bytesRead > 0)
-//            {   
-//                out.write(buffer, 0, bytesRead);
-//                totalSent += bytesRead;
-//                System.out.println("sent " + totalSent);
-//            }   
-//        }
-
-        sock.close();
-
-        //System.out.println("Sent " + totalSent + " bytes in "
-        //        + (System.currentTimeMillis() - time) + "ms.");
-        
-        
-        
-
+		while ((userInput = stdIn.readLine()) != null) {
+			out.println(userInput);
+			System.out.println("echo: " + in.readLine());
+		}
+		in.close();
+		out.close();
+		stdIn.close();
     }
 }
