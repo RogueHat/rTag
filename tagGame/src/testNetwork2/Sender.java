ceba3b9c78b05fa31ecba54c178c12cc3b4727e3
CarloSchool <carlomiguelh@yahoo.com>
2013-02-15 13:14:33 -0600

    asdjklfhjkadf

--- tagGame/src/testNetwork2/Sender.java
+++ tagGame/src/testNetwork2/Sender.java
@@ -10,10 +10,9 @@ public class Sender {
 		DatagramSocket datagramSocket = new DatagramSocket();
 
 		byte[] buffer = "0123456789".getBytes();
-		InetAddress receiverAddress = InetAddress.getLocalHost();
+		InetAddress[] receiverAddress = InetAddress.getAllByName("10.9.30.37");
 
-		DatagramPacket packet = new DatagramPacket(
-		        buffer, buffer.length, receiverAddress, 80);
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length, receiverAddress[0], 80);
 		datagramSocket.send(packet);
 	}
 }
