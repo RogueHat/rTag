7bd410745ecb1a0c9073d5b8b314ce844e10772d
carlo <carlo@TIMEFREEZE>
2013-02-17 11:50:35 -0600

    somewhat successful input for testGame

--- TestGame/src/graphics/GraphicsRunner.java
+++ TestGame/src/graphics/GraphicsRunner.java
@@ -1,7 +1,8 @@
 package graphics;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.TreeMap;
 
 import javax.swing.JFrame;
@@ -10,22 +11,60 @@ import javax.swing.JFrame;
 public class GraphicsRunner extends JFrame {
 	private static final int WIDTH = 800;
 	private static final int HEIGHT = 800;
+	private static int spd=0;
+	private Map<String,Player> players = new TreeMap<String,Player>();
 
-	public GraphicsRunner(Map<String, Player> players) {
-		super("MAKE YOUR OWN SHAPE");
-
+	public GraphicsRunner() {
+		super("Graphics Runner");
 		setSize(WIDTH, HEIGHT);
+		
+		players.put("10.26.6.11", new Player());
+		
 		getContentPane().add(new MovingPlayer(players));
-
+		Event e = new Event();
+		this.addKeyListener(e);
+		
+		
+		
 		setVisible(true);
-
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
+	
+	public class Event implements KeyListener{
+
+		@Override
+		public void keyPressed(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			keyTyped(arg0);
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			Player pl = players.get("10.26.6.11");;
+			
+			switch(arg0.getKeyCode()){
+			case KeyEvent.VK_UP: 	pl.setY(-spd);	spd++;	break;
+			case KeyEvent.VK_DOWN: 	pl.setY(spd);	spd++;	break;
+			case KeyEvent.VK_LEFT: 	pl.setX(-spd);	spd++;	break;
+			case KeyEvent.VK_RIGHT:	pl.setX(spd); 	spd++;	break;
+			default: spd=0; break;
+			}
+		}
+		
+	}
+	
 
 	public static void main(String args[]) {
-		Map<String, Player> meh = new TreeMap<String, Player>();
-		meh.put("10.26.6.11", new Player());
+		//Map<String, Player> meh = new TreeMap<String, Player>();
+		//meh.put("10.26.6.11", new Player());
 		@SuppressWarnings("unused")
-		GraphicsRunner run = new GraphicsRunner(meh);
+		GraphicsRunner run = new GraphicsRunner();
 	}
 }
