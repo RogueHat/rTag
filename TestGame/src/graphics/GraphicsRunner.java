679f0bc96884c12f708ac62dbeacbc84214c3035
carlo <carlo@TIMEFREEZE>
2013-02-17 22:40:38 -0600

    nothing for now

--- TestGame/src/graphics/GraphicsRunner.java
+++ TestGame/src/graphics/GraphicsRunner.java
@@ -17,47 +17,10 @@ public class GraphicsRunner extends JFrame {
 	public GraphicsRunner() {
 		super("Graphics Runner");
 		setSize(WIDTH, HEIGHT);
-		
 		players.put("10.26.6.11", new Player());
-		
 		getContentPane().add(new MovingPlayer(players));
-		Event e = new Event();
-		this.addKeyListener(e);
-		
-		
-		
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-	
-	public class Event implements KeyListener{
-
-		@Override
-		public void keyPressed(KeyEvent arg0) {
-			// TODO Auto-generated method stub
-			keyTyped(arg0);
-		}
-
-		@Override
-		public void keyReleased(KeyEvent arg0) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void keyTyped(KeyEvent arg0) {
-			// TODO Auto-generated method stub
-			Player pl = players.get("10.26.6.11");;
-			
-			switch(arg0.getKeyCode()){
-			case KeyEvent.VK_UP: 	pl.setY(-spd);	spd++;	break;
-			case KeyEvent.VK_DOWN: 	pl.setY(spd);	spd++;	break;
-			case KeyEvent.VK_LEFT: 	pl.setX(-spd);	spd++;	break;
-			case KeyEvent.VK_RIGHT:	pl.setX(spd); 	spd++;	break;
-			default: spd=0; break;
-			}
-		}
-		
 	}
 	
 
