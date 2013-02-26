1b97bf29413906380286e027e383cf0a1a8623d4
CarloSchool <carlomiguelh@yahoo.com>
2013-02-26 13:27:54 -0600

    testServer

---
+++ tagGame/src/server/Server.java
@@ -0,0 +1,16 @@
+package server;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+import network.Network;
+
+public class Server {
+	public static void main (String[]args){
+		Network net = new Network();
+		Map<String, String> players = new TreeMap<String, String>();
+		while(true){
+			
+		}
+	}
+}
